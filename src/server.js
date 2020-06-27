2a2c2d7ce800e317d35df1993351dbbba3998344
satishgupta07 <satishgupta.cse01@gmail.com>
2020-06-28 03:40:05 +0530

    creating and fetching article works
    Signed-off-by: satishgupta07 <satishgupta.cse01@gmail.com>

--- src/server.js
+++ src/server.js
@@ -1,16 +1,15 @@
 const express = require('express')
-
 const { db } = require('./db/models')
 const { usersRoute } = require('./routes/users')
 const { postsRoute } = require('./routes/posts')
 
 const app = express()
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
 
-db.sync()
 app.use('/api/users', usersRoute)
 app.use('/api/posts', postsRoute)
 app.use('/', express.static(__dirname + '/public'))
-
 db.sync()
   .then(() => {
     app.listen(8383, () => {
